39ec82fb9756cc711d1ab902e4816df877d53294
lzear <antoine.clausse@gmail.com>
2020-10-18 21:39:20 +0200

    chore: update the entire configuration (tsconfig, udpate dependencies, rollup, eslint...)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
