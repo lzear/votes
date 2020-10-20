36a4d968cc683c87aa1476178ba183c835e0b6f9
lzear <antoine.clausse@gmail.com>
2020-10-21 00:42:06 +0200

    build: update rollup and add sideEffects:false

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import * as _ from 'lodash'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
@@ -9,14 +8,25 @@ import pkg from './package.json'
 
 const libraryName = 'votes'
 
-const config = ({ input, output }) => ({
-  input,
-  output,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+const rollupConfig = {
+  input: `src/${libraryName}.ts`,
   external: ['lodash'],
-  watch: {
-    include: 'src/**',
-  },
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+      globals: { lodash: '_' },
+      name: 'votes',
+      sourcemap: true,
+    },
+    {
+      file: pkg.main,
+      format: 'umd',
+      globals: { lodash: '_' },
+      name: 'votes',
+      sourcemap: true,
+    },
+  ],
   plugins: [
     // Allow json resolution
     json(),
@@ -32,54 +42,7 @@ const config = ({ input, output }) => ({
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-})
-export default [
-  config({
-    input: `src/${libraryName}.ts`,
-    name: _.camelCase(libraryName),
-    output: [
-      {
-        file: pkg.main,
-        name: 'votes',
-        format: 'umd',
-        sourcemap: true,
-        globals: {
-          lodash: '_',
-        },
-      },
-      {
-        file: pkg.module,
-        name: 'votes',
-        format: 'es',
-        sourcemap: true,
-        globals: {
-          lodash: '_',
-        },
-      },
-    ],
-  }),
-  config({
-    input: 'src/utils/index.ts',
-    name: 'utils',
-    output: [
-      {
-        file: 'dist/utils.umd.js',
-        name: 'utils',
-        format: 'umd',
-        sourcemap: true,
-        globals: {
-          lodash: '_',
-        },
-      },
-      {
-        file: 'dist/utils.es5.js',
-        name: 'utils',
-        format: 'es',
-        sourcemap: true,
-        globals: {
-          lodash: '_',
-        },
-      },
-    ],
-  }),
-]
+  watch: { include: 'src/**' },
+}
+
+export default rollupConfig
