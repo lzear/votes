3b035628835176bcb755cb5bfd5d505fab70b0c6
lzear <antoine.clausse@gmail.com>
2021-06-10 08:48:51 +0200

    ci: add project-token param to Codacy coverage report script

--- tools/codacy-cov.sh
+++ tools/codacy-cov.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r ./coverage/lcov.info
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r ./coverage/lcov.info --project-token "$CODACY_PROJECT_TOKEN"
