11efcb0bab467de4e2ed14e48500d0c0a4e15410
lzear <antoine.clausse@gmail.com>
2021-06-10 09:02:01 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
