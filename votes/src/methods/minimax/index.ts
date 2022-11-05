861cbad85d4e073cfac53cae4eb6b3a2f88e2021
lzear <antoine.clausse@gmail.com>
2022-11-05 10:56:05 +0100

    fix: Filter out duel against itself in Minimax!

--- votes/src/methods/minimax/index.ts
+++ votes/src/methods/minimax/index.ts
@@ -8,10 +8,10 @@ export enum MinimaxVariant {
 }
 
 const scoreXY = {
+  [MinimaxVariant.Margins]: (xOverY: number, yOverX: number) => xOverY - yOverX, // default
+  [MinimaxVariant.PairwiseOpposition]: (xOverY: number) => xOverY,
   [MinimaxVariant.WinningVotes]: (xOverY: number, yOverX: number) =>
     xOverY > yOverX ? xOverY : 0,
-  [MinimaxVariant.Margins]: (xOverY: number, yOverX: number) => xOverY - yOverX,
-  [MinimaxVariant.PairwiseOpposition]: (xOverY: number) => xOverY,
 }
 
 const computeScores = (
@@ -19,11 +19,13 @@ const computeScores = (
   variant: MinimaxVariant,
 ): ScoreObject => {
   const s: ScoreObject = {}
-  for (const [k, c] of matrix.candidates.entries()) {
-    s[c] = -Math.max(
-      ...matrix.array[k].map((v, k2) =>
-        scoreXY[variant](matrix.array[k2][k], v),
-      ),
+  for (const [c1Index, candidate] of matrix.candidates.entries()) {
+    s[candidate] = -Math.max(
+      ...matrix.array[c1Index]
+        .map((v, c2Index) =>
+          scoreXY[variant](matrix.array[c2Index][c1Index], v),
+        )
+        .filter((_, c2Index) => c2Index !== c1Index),
     )
   }
   return s
