e21be3d0e978c4b4351839408767454ab93fe036
lzear <antoine.clausse@gmail.com>
2023-02-07 00:48:48 +0100

    chore: update hsluv to v1 syntax

--- demo/src/traffic-color.ts
+++ demo/src/traffic-color.ts
@@ -1,9 +1,23 @@
-import hsluv from 'hsluv'
+import { Hsluv } from 'hsluv'
 import _ from 'lodash'
 
+const hsluvToHex = ([h, s, l]: [h: number, s: number, l: number]): string => {
+  const hsluv = new Hsluv()
+  hsluv.hsluv_h = h
+  hsluv.hsluv_s = s
+  hsluv.hsluv_l = l
+  hsluv.hsluvToHex()
+  return hsluv.hex
+}
+
 export const trafficColor = (score: number, s: number, l: number): string => {
-  return hsluv.hsluvToHex([score * 150, s, l])
+  const hsluv = new Hsluv()
+  hsluv.hsluv_h = score * 150
+  hsluv.hsluv_s = s
+  hsluv.hsluv_l = l
+  hsluv.hsluvToHex()
+  return hsluvToHex([score * 150, s, l])
 }
 
 export const rainbow = (count: number, s: number, l: number): string[] =>
-  _.range(count).map((v) => hsluv.hsluvToHex([(360 * v) / count, s, l]))
+  _.range(count).map((v) => hsluvToHex([(360 * v) / count, s, l]))
