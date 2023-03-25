fe82ace9814cd028e6b972386c7beabb90ddaeb2
lzear <antoine.clausse@gmail.com>
2023-03-25 11:19:04 +0100

    build: add build as IIFE module
    #96

--- votes/tsup.config.ts
+++ votes/tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
-  format: ['esm', 'cjs'],
+  format: ['esm', 'cjs', 'iife'],
   clean: true,
   target: 'esnext',
   dts: true,
   minify: true,
+  globalName: 'votes',
 })
