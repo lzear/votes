1505d40c1c39f02bde651b9a2ba6ee0677ba7b9f
lzear <antoine.clausse@gmail.com>
2026-06-01 21:56:32 +0200

    chore: update configs

--- votes/typedoc.cjs
+++ votes/typedoc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   out: './docs',
-  includes: './src/index.ts',
   entryPoints: ['./src/index.ts'],
   exclude: ['**/*.test.ts', '**/test/**/*', '../node_modules/**/*'],
   externalPattern: ['../node_modules/**/*'],
   excludeExternals: true,
   excludePrivate: true,
+  excludeInternal: true,
+  validation: { notExported: false },
 }
