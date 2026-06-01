5d39f79ce924fe51a96e75d73e1ff9269fc4cf98
lzear <antoine.clausse@gmail.com>
2026-06-01 21:56:32 +0200

    chore: rework BallotScoreMethod

--- votes/src/classes/ballot-score-method.ts
+++ votes/src/classes/ballot-score-method.ts
@@ -1,30 +1,14 @@
-import type { Ballot, Matrix } from '../types'
-import { matrixFromBallots } from '../utils/make-matrix'
-import type { Matrixer } from './matrix-score-method'
-import { ScoreMethod, type Scorer } from './score-method'
-
+import type { ScoreObject } from '../types'
+import { scoresToRanking } from '../utils'
+import { BallotMethod } from './ballot-method'
+import { type Scorer } from './score-method'
 export abstract class BallotScoreMethod<C extends string>
-  extends ScoreMethod<C>
-  implements Scorer<C>, Matrixer<C>
+  extends BallotMethod<C>
+  implements Scorer<C>
 {
-  public static readonly needsBallot = true
-  protected readonly ballots: Ballot<C>[]
-  private _matrix?: Matrix<C>
+  public abstract scores(): ScoreObject<C>
 
-  constructor({
-    ballots,
-    candidates,
-  }: {
-    ballots: Ballot<C>[]
-    candidates: C[]
-  }) {
-    super(candidates)
-    this.ballots = ballots
-  }
-
-  get matrix(): Matrix<C> {
-    this._matrix ??= matrixFromBallots(this.ballots, this.candidates)
-
-    return this._matrix
+  public ranking(): C[][] {
+    return scoresToRanking(this.scores())
   }
 }
