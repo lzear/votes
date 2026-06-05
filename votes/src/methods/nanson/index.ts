d8fcff0737f1c5b7ebd6c153a13d7ad7d6f5c769
lzear <antoine.clausse@gmail.com>
2026-06-05 22:07:30 +0200

    chore: simplify Nanson

--- votes/src/methods/nanson/index.ts
+++ votes/src/methods/nanson/index.ts
@@ -1,23 +1,13 @@
 import { sum } from 'lodash-es'
 import type { QE } from '../../classes/round-ballot-method'
-import { TbEliminateLast } from '../../classes/round-ballot-method-tb'
-import type { ScoreObject } from '../../types'
-import { scoresToRanking } from '../../utils'
+import { RoundBallotMethodTb } from '../../classes/round-ballot-method-tb'
 import { config } from '../../utils/config'
 import { Borda } from '../borda'
 
 /**
  * #### Wikipedia: [Nanson's method](https://en.wikipedia.org/wiki/Nanson%27s_method)
  */
-export class Nanson<C extends string> extends TbEliminateLast<C> {
-  protected oneRound(candidates: C[]): {
-    ranking: C[][]
-    scores: ScoreObject<C>
-  } {
-    const scores = new Borda({ candidates, ballots: this.ballots }).scores()
-    return { ranking: scoresToRanking(scores), scores }
-  }
-
+export class Nanson<C extends string> extends RoundBallotMethodTb<C> {
   protected round(candidates: C[]): QE<C> {
     const scores = new Borda({ candidates, ballots: this.ballots }).scores()
     const values = Object.values(scores)
@@ -32,6 +22,6 @@ export class Nanson<C extends string> extends TbEliminateLast<C> {
     if (qualified.length === 0)
       return { eliminated: candidates, qualified: [], scores }
 
-    return { eliminated, qualified, scores }
+    return { qualified, eliminated, scores }
   }
 }
